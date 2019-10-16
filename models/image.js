072a13fdf2a2fa6ec146d4300883e3b1f5d0fcf4
Timothy Kaing <kaing.timothy@gmail.com>
2019-10-16 14:25:48 -0700

    added createdAt attribute

--- models/image.js
+++ models/image.js
@@ -2,11 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ImageSchema = new Schema({
+    createdAt: { type: Date },
     name: { type: String, default: "n/a" },
     camera: { type: String, required: true },
     lens: { type: String, default: "n/a" },
     film: { type: String, default: "n/a" },
     image: { type: String, required: true }
 });
+
+ImageSchema.pre('save', function(next) {
+    const now = new Date();
+    if (!this.createdAt) {
+        this.createdAt= now;
+    }
+
+    next();
+})
 
 module.exports =  mongoose.model('Image', ImageSchema)
