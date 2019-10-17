4cec76a30193c2f7836a2521688d9bb59fa0bce0
Timothy Kaing <kaing.timothy@gmail.com>
2019-10-16 20:02:33 -0700

    render gallery on index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const Image = require('../models/image')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Lomodomo' });
+router.get('/',  async function(req, res, next) {
+  // res.render('index', { title: 'Lomodomo' });
+
+  try {
+    Image.find({})
+      .then(images => {
+        res.render('images-index', { images });
+      })
+  }
+  catch(error) {
+    console.log(error.message);
+    res.send(500);
+  }
+  
 });
 
 module.exports = router;
