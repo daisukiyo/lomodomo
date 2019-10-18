ad594f5f461de8151f9a95704a1867701c08d081
Timothy Kaing <kaing.timothy@gmail.com>
2019-10-17 20:09:40 -0700

    create show single image route

--- controllers/image/get.js
+++ controllers/image/get.js
@@ -5,3 +5,16 @@ module.exports.newImage = function(req, res) {
     
     // res.send('respond with a resource');
 }
+
+module.exports.singleImage = async function(req, res) {
+    try {
+        var imageObject = await Image.findById(req.params.id)
+
+    }
+    catch(err) {
+        console.log(err)
+    }
+    res.render('single-image', { image: imageObject });
+    
+    // res.send('respond with a resource');
+}
