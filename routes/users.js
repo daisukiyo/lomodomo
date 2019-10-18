4a056276190af79e83cc917da7f1a3143a83afec
Timothy Kaing <kaing.timothy@gmail.com>
2019-10-18 01:58:04 -0700

    created logout route for user

--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,7 @@ var controller = require('../controllers/auth');
 
 router.get('/signup', controller.get.newUser);
 
+router.get('/logout', controller.get.userLogout)
+
 router.post('/', controller.post.postUser)
 module.exports = router;
