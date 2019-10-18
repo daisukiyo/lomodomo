cf0a47eb4a766771bc4a646c4eafd20a013ef25a
Timothy Kaing <kaing.timothy@gmail.com>
2019-10-17 20:31:45 -0700

    add mongo env

--- data/lomodomo-db.js
+++ data/lomodomo-db.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 assert = require("assert");
 
-const url = "mongodb://localhost/lomodomo-db";
+const url = process.env.MONGODB_URI ||"mongodb://localhost/lomodomo-db";
 mongoose.Promise = global.Promise;
 mongoose.connect(
   url,
