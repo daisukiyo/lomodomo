b454a923bd5fb6169911171cbbec355b51a844c8
Timothy Kaing <kaing.timothy@gmail.com>
2019-10-18 02:52:54 -0700

    login routes now redirect and authenticate

--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,10 @@ router.get('/signup', controller.get.newUser);
 
 router.get('/logout', controller.get.userLogout)
 
+router.get('/login', controller.get.userLogin)
+
+router.post('/login', controller.post.userAuthenticate)
+
 router.post('/', controller.post.postUser)
+
 module.exports = router;
