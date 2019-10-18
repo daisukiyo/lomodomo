9191cc04e29537b738188350d9246c953fb1694e
Timothy Kaing <kaing.timothy@gmail.com>
2019-10-18 02:52:29 -0700

    set up user authentication

--- controllers/auth/post.js
+++ controllers/auth/post.js
@@ -14,3 +14,32 @@ module.exports.postUser = async (req, res) => {
         res.send(500);
     }
 }
+
+module.exports.userAuthenticate = async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    try {
+        // make user the result of the successful promise
+        const user = await User.findOne({ username }, "username password")
+
+        if(!user) {
+            return res.status(401).send({ message: 'wrong username or password' });
+        }
+
+        // promisfy in the future
+        user.comparePassword(password, (err, isMatch) => {
+            if(!isMatch) {
+                return res.status(401).send({ message: "Wrong Username or password" }); 
+            }
+            const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, {
+                expiresIn: "60 days"
+            });
+        
+            res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
+            res.redirect("/");
+        })
+    }
+    catch(err) {
+        console.log(err);
+    }
+}
