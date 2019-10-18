1a5c2a73ef04abff1b47c01968709e5e19496dcc
Timothy Kaing <kaing.timothy@gmail.com>
2019-10-18 01:47:51 -0700

    generate + configure JWT + cookie

--- controllers/auth/post.js
+++ controllers/auth/post.js
@@ -1,9 +1,12 @@
 const User = require("../../models/user");
+const jwt = require('jsonwebtoken');
 
 module.exports.postUser = async (req, res) => {
     try {
-        await User.create(req.body);
         console.log(req.body);
+        const user = await User.create(req.body);
+        var token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
+        res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
         res.send(200);
     }
     catch(error) {
